f4cbd76eab88fdf6b2cb124193c83c102532fcfa
= <=>
2020-06-14 14:34:16 +0530

    initial commit

---
+++ Public/data/movies_name.js
@@ -0,0 +1,77 @@
+var movie_list = []
+movie_list.push({
+
+    // href: "http://www.localhost:5400/movies?title=Money+Heist",
+    src: "https://1.bp.blogspot.com/-EWW_Fj5-R6A/XtC-_jBfTqI/AAAAAAAAAXQ/GfRVExSqH8UWKTmz5nmo8zsZlquXjS0sQCK4BGAsYHg/w480/Money-Heist-season-4-1.jpg",
+    title: "Money Heist (Season 1 &#8211; 4) Dual Audio {English-Spanish} 720p WeB-DL HD [350MB]",
+    discription: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a cr&#8230;",
+    seasons: [13, 9, 8, 8],
+    disc_complete: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. A group of unique robbers assaults the Factory of Moneda and Timbre to carry out the most perfect robbery in the history of Spain and take home 2.4 billion euros.",
+    movie_name: "Money Heist",
+    type: "BollyWood Movies",
+    links: [["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400","http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"]]
+    // links = {{}}
+});
+
+movie_list.push({
+
+    // href: "http://www.localhost:5400/movies?title=Money+Heist",
+    src: "https://1.bp.blogspot.com/-EWW_Fj5-R6A/XtC-_jBfTqI/AAAAAAAAAXQ/GfRVExSqH8UWKTmz5nmo8zsZlquXjS0sQCK4BGAsYHg/w480/Money-Heist-season-4-1.jpg",
+    title: "Money Heist (Season 1 &#8211; 4) Dual Audio {English-Spanish} 720p WeB-DL HD [350MB]",
+    discription: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a cr&#8230;",
+    seasons: [13, 9, 8, 8],
+    disc_complete: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. A group of unique robbers assaults the Factory of Moneda and Timbre to carry out the most perfect robbery in the history of Spain and take home 2.4 billion euros.",
+    movie_name: "Money Heist",
+    type: "Bollywood Series",
+    links: [["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400","http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"]]
+});
+movie_list.push({
+    // href: "http://www.localhost:5400/movies?title=Money+Heist",
+    src: "https://1.bp.blogspot.com/-EWW_Fj5-R6A/XtC-_jBfTqI/AAAAAAAAAXQ/GfRVExSqH8UWKTmz5nmo8zsZlquXjS0sQCK4BGAsYHg/w480/Money-Heist-season-4-1.jpg",
+    title: "Money Heist (Season 1 &#8211; 4) Dual Audio {English-Spanish} 720p WeB-DL HD [350MB]",
+    discription: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a cr&#8230;",
+    seasons: [13, 9, 8, 8],
+    disc_complete: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. A group of unique robbers assaults the Factory of Moneda and Timbre to carry out the most perfect robbery in the history of Spain and take home 2.4 billion euros.",
+    movie_name: "Money Heist",
+    type: "Hollywood movies",
+    links: [["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400","http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"]]
+});
+movie_list.push({
+    href: "http://www.localhost:5400/movies?title=Money+Heist",
+    src: "https://1.bp.blogspot.com/-EWW_Fj5-R6A/XtC-_jBfTqI/AAAAAAAAAXQ/GfRVExSqH8UWKTmz5nmo8zsZlquXjS0sQCK4BGAsYHg/w480/Money-Heist-season-4-1.jpg",
+    title: "Money Heist (Season 1 &#8211; 4) Dual Audio {English-Spanish} 720p WeB-DL HD [350MB]",
+    discription: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a cr&#8230;",
+    seasons: [13, 9, 8, 8],
+    disc_complete: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. A group of unique robbers assaults the Factory of Moneda and Timbre to carry out the most perfect robbery in the history of Spain and take home 2.4 billion euros.",
+    movie_name: "Money Heist",
+    type: "Hollywood Series",
+    links: [["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400","http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"],
+    ["http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400", "http://localhost:5400"]]
+});
+
+movie_list.push({
+//href: "https://stackoverflow.com/questions/13395742/can-not-get-css-file",
+src: "/images/Doraemon_renderImproved.png",
+title: "Doremon and the little adventure",
+discription:"Gifted with doraemon lets see how nobita fucks suzuka",
+disc_complete:"suzuka with sexy pussy is gonna be fucked by dekisuki but wait nobita will grab her pusy and fuck her with the help of doreamon ",
+type:"animated movie",
+seasons:[2],
+movie_name:"Doraemon",
+links:["http://www.cyberciti.biz/faq/stop-process-ubuntu-linux-command/",""]
+})
+
+
+
+module.exports = movie_list;
---
+++ server.js
@@ -0,0 +1,123 @@
+// const fs = require('fs')
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5400;
+// const bodyParser = require("body-parser");
+// const session = require('express-session');
+// const passport = require('passport');
+// const cors = require('cors')
+app.use(express.static(path.join(__dirname, "Public")));
+app.set('view engine', 'ejs');
+var corsOptions = {
+    origin: 'http://localhost:5400'
+}
+
+// app.use(cors());
+
+// app.use(cookie_parser());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+// extended: true
+// }));
+
+var movie_list = require("./Public/data/movies_name");
+
+app.get("/movies", (req, res) => {
+    var ind = req.query.index || 0;
+    var current_url = req.get('host');
+    var mov_title = req.query.title.toLowerCase();
+    for (var i = 0; i < movie_list.length; i++) {
+        console.log(movie_list[i].movie_name);
+        if (mov_title == movie_list[i].movie_name.toLowerCase()) {
+            // console.log("found");
+            var dict = Object.assign({ current_url: current_url, index: ind }, movie_list[i]);
+            res.render("indivi_movie", dict);
+            return ;
+        }
+    }
+    res.render("index", { alert: "no book found", current_url: current_url, index: ind, movie_list: movie_list });
+})
+
+
+
+
+app.use("/bollywood_series", (req, res) => {
+    var ind = req.query.index || 0;
+    var final_list = [];
+    for (var i = 0; i < movie_list.length; i++) {
+        console.log(movie_list[i].movie_name);
+        if (movie_list[i].type.toLowerCase() == "bollywood series") {
+            final_list.push(movie_list[i]);
+        }
+    }
+    var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, type: "bollywood_series" }, { movie_list: final_list });
+    res.render("specific", dict);
+})
+
+app.use("/bollywood_movies", (req, res) => {
+    var ind = req.query.index || 0;
+    var final_list = [];
+    for (var i = 0; i < movie_list.length; i++) {
+        // console.log(movie_list[i].movie_name);
+        if (movie_list[i].type.toLowerCase() == "bollywood movies") {
+            final_list.push(movie_list[i]);
+        }
+    }
+    var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, type: "bollywood_movies" }, { movie_list: final_list });
+    // console.log(dict);
+    res.render("specific", dict);
+})
+
+app.use("/hollywood_series", (req, res) => {
+    var ind = req.query.index || 0;
+    var final_list = [];
+    for (var i = 0; i < movie_list.length; i++) {
+
+        if (movie_list[i].type.toLowerCase() == "hollywood series") {
+            final_list.push(movie_list[i]);
+        }
+    }
+    var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, type: "hollywood_series" }, { movie_list: final_list });
+    res.render("specific", dict);
+})
+
+app.use("/hollywood_movies", (req, res) => {
+    var ind = req.query.index || 0;
+    var final_list = [];
+    for (var i = 0; i < movie_list.length; i++) {
+
+        if (movie_list[i].type.toLowerCase() == "hollywood movies") {
+            final_list.push(movie_list[i]);
+        }
+    }
+    var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, type: "hollywood_movies" }, { movie_list: final_list });
+    res.render("specific", dict);
+})
+
+var mv_list = [];
+
+
+
+app.use("/ind/", (req, res) => {
+    var ind = req.query.index || 0;
+    var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, alert: undefined }, { movie_list: movie_list });
+    res.render("index", dict);
+})
+
+app.use("/", (req, res) => {
+  var ind = req.query.index || 0;
+var current_url = req.get('host');
+    var dict = Object.assign({ index: ind, current_url: current_url, alert: undefined }, { movie_list: movie_list });
+    res.render("index", dict);
+})
+
+app.listen(port, () => {
+    console.log(`listining on port ${port}`);
+});
+
